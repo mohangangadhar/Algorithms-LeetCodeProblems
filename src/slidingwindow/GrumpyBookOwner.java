eeb430f064cea173b8f6119541d87efc0c63693d
mgudey <mohan.gudey@nike.com>
2019-12-16 17:15:20 -0800

    Grumpy Book Owner

---
+++ src/slidingwindow/GrumpyBookOwner.java
@@ -0,0 +1,51 @@
+package slidingwindow;
+
+//        Today, the bookstore owner has a store open for customers.length minutes.  Every minute, some number of customers (customers[i]) enter the store, and all those customers leave after the end of that minute.
+//
+//        On some minutes, the bookstore owner is grumpy.  If the bookstore owner is grumpy on the i-th minute, grumpy[i] = 1, otherwise grumpy[i] = 0.  When the bookstore owner is grumpy, the customers of that minute are not satisfied, otherwise they are satisfied.
+//
+//        The bookstore owner knows a secret technique to keep themselves not grumpy for X minutes straight, but can only use it once.
+//
+//        Return the maximum number of customers that can be satisfied throughout the day.
+//
+//
+//
+//        Example 1:
+//
+//        Input: customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], X = 3
+//        Output: 16
+//        Explanation: The bookstore owner keeps themselves not grumpy for the last 3 minutes.
+//        The maximum number of customers that can be satisfied = 1 + 1 + 1 + 1 + 7 + 5 = 16.
+//
+//
+//        Note:
+//
+//        1 <= X <= customers.length == grumpy.length <= 20000
+//        0 <= customers[i] <= 1000
+//        0 <= grumpy[i] <= 1
+
+public class GrumpyBookOwner {
+    public static void main(String... args) {
+        int[] customers = {1, 0, 1, 2, 1, 1, 7, 5}, grumpy = {0, 1, 0, 1, 0, 1, 0, 1};
+        int X = 3;
+        System.out.println(getSatisfiedCustomersCount(customers, grumpy, X));
+    }
+
+    private static int getSatisfiedCustomersCount(int[] customers, int[]grumpy, int x) {
+
+        int satisfiedCustomersCount = 0, begin = 0;
+        int maxValue = 0;
+        int y = x;
+        for(int j =0; j< customers.length; j++){
+            if (grumpy[j] == 1 || y < x) y--;
+
+            if(y < 0 && grumpy[begin++] == 1 ) {
+                y++;
+                satisfiedCustomersCount = Math.max(satisfiedCustomersCount, maxValue);
+                maxValue = 0;
+            }
+            maxValue = customers[j] + maxValue;
+        }
+        return maxValue;
+    }
+}
