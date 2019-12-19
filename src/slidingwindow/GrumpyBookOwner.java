118f703802fe6c211b48a29ebb0389cfbcaa29b4
mgudey <mohan.gudey@nike.com>
2019-12-19 07:20:32 -0800

    Grumpy book owner

--- src/slidingwindow/GrumpyBookOwner.java
+++ src/slidingwindow/GrumpyBookOwner.java
@@ -1,5 +1,6 @@
 package slidingwindow;
 
+
 //        Today, the bookstore owner has a store open for customers.length minutes.  Every minute, some number of customers (customers[i]) enter the store, and all those customers leave after the end of that minute.
 //
 //        On some minutes, the bookstore owner is grumpy.  If the bookstore owner is grumpy on the i-th minute, grumpy[i] = 1, otherwise grumpy[i] = 0.  When the bookstore owner is grumpy, the customers of that minute are not satisfied, otherwise they are satisfied.
@@ -26,26 +27,39 @@ package slidingwindow;
 
 public class GrumpyBookOwner {
     public static void main(String... args) {
+        Validate validate = new Validate();
         int[] customers = {1, 0, 1, 2, 1, 1, 7, 5}, grumpy = {0, 1, 0, 1, 0, 1, 0, 1};
         int X = 3;
-        System.out.println(getSatisfiedCustomersCount(customers, grumpy, X));
+        validate.assertInt(16, getSatisfiedCustomersCount(customers, grumpy, X));
+        validate.assertInt(18, getSatisfiedCustomersCount(new int [] {1, 0, 1, 2, 1, 1, 7, 5},new int [] {0, 0, 0, 0, 0, 0, 0, 1}, X));
+        validate.assertInt(17, getSatisfiedCustomersCount(new int [] {1, 0, 1, 2, 1, 1, 7, 5},new int [] {1, 0, 0, 0, 0, 0, 0, 1}, X));
+        validate.assertInt(18, getSatisfiedCustomersCount(new int [] {1, 0, 1, 2, 1, 1, 7, 5},new int [] {0, 0, 0, 0, 0, 0, 0, 0}, X));
+        validate.assertInt(17, getSatisfiedCustomersCount(new int [] {1, 0, 1, 2, 1, 1, 7, 5},new int [] {1, 0, 0, 0, 0, 0, 0, 1}, 1));
+        validate.assertInt(3, getSatisfiedCustomersCount(new int [] {3},new int [] {1}, 1));
     }
 
-    private static int getSatisfiedCustomersCount(int[] customers, int[]grumpy, int x) {
+    private static int getSatisfiedCustomersCount(int[] customers, int[] grumpy, int x) {
 
-        int satisfiedCustomersCount = 0, begin = 0;
-        int maxValue = 0;
-        int y = x;
-        for(int j =0; j< customers.length; j++){
-            if (grumpy[j] == 1 || y < x) y--;
-
-            if(y < 0 && grumpy[begin++] == 1 ) {
-                y++;
-                satisfiedCustomersCount = Math.max(satisfiedCustomersCount, maxValue);
-                maxValue = 0;
+        int maxValue = 0, maxHappySum = 0;
+        int oneSum = 0;
+        for (int j = 0; j < customers.length; j++) {
+            if (grumpy[j] == 0) {
+                maxHappySum = customers[j] + maxHappySum;
+                customers[j] = 0;
             }
-            maxValue = customers[j] + maxValue;
         }
-        return maxValue;
+        for (int j = 0; j < x; j++) {
+            if(grumpy[j] == 1)
+                oneSum = oneSum + customers[j];
+        }
+        maxValue = oneSum;
+
+        for (int j = x; j < customers.length; j++) {
+            oneSum = customers[j] + oneSum - customers[j-x];
+            if(maxValue < oneSum){
+                maxValue = oneSum;
+            }
+        }
+        return maxHappySum + maxValue;
     }
 }
